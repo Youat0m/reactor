51577b9d74fdfc7b884ae226d5714cda52b0fe00
youAt0m <senkaved@gmail.com>
2025-06-18 15:57:51 +0300

    neutrons (now work)

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass, field
 import math
 import os
-from random import randint
+from random import randint, random
 import time
+from types import NoneType
 import numpy as np
 import pygame as pg
 from numba import jit, njit
@@ -10,10 +11,13 @@ import sys
 
 
 drawList = []
+Neutron_list=[]
 zone=(1024,512)
 FUEL_ROD_SIZE = 10
 XENON_CELL_SIZE = 10
-
+STEP = 1/4
+ROD_COUNT = 15
+CLOCK_TIME = 10
 class MatObject():
     n:float 
 
@@ -32,38 +36,72 @@ class MatObject():
 
 @dataclass
 class NeutronRay():
-    originObj:MatObject
+    originObjcolor:int
     pitc:float
     origin:np.ndarray
     isFast:bool
-    def raycast(self):
-        #raycast
 
-        return ()
-    def throw(self):
+   
+
+    def __post_init__(self):
+        self.originVec = np.array((math.cos(self.pitc),math.sin(self.pitc)))*STEP
+        self.pos = origin
+        Neutron_list.append(self)
+    def raycast(self, screen:pg.surface.Surface):
+        # print("a")
+        color = screen.get_at((int(self.pos[0]),int(self.pos[1]))).g 
+        while color == self.originObjcolor:
+            self.pos += self.originVev
+            color = screen.get_at((int(self.pos[0])),int(self.pos[1])).g 
+        lenght = np.linalg.norm(self.pos-self.origin)
+        matObj = findObj(color)
+        self.originObjcolor = color 
+        pg.draw.circle(screen,(250,0,0),(int(self.pos[0]),int(self.pos[1])),10)
+        
+        return matObj, lenght
+    def throw(self, screen:pg.surface.Surface):
         is_interacte = False
-        while(not isinstance):
-            #matObj, lenght = raytrace()
-            #if out of border:
-            #   break
-            #is_interacted = interact()
-            pass
+        while(not is_interacte):
+            matObj, lenght = self.raycast(screen)
+            if type(matObj) == NoneType:
+                break 
+            is_interacted = matObj.interact(self, lenght,(self.pos-self.origin)/2)
+            print(type(matObj))
+        drawList.append(self)
+        Neutron_list.remove(self)
+    def draw(self,screen):
+        pg.draw.line(screen, (255,255,255),(int(self.origin[0]), int(self.origin[1])),(int(self.pos[0]),int(self.pos[1])))
+        drawList.remove(self)
 
 class WaterField():
     n = 1
     def __init__(self,xsize,ysize):
-        self.temp = np.ones((xsize,ysize))
+        self.temp = np.full((xsize,ysize),200.0)
         drawList.append(self)
 
 
     def randgen(self):
         _randgen(self.temp)
 
-    def interact(self, ray:NeutronRay):
-        if(ray.isFast):
-            return ()
-        else:
-            pass
+    def substract(self):
+        self.temp = self.temp - np.full_like(self.temp,1/CLOCK_TIME)
+        self.temp[np.where(self.temp<0)]=1
+        pass
+
+    def interact(self, ray:NeutronRay, lenght, pos:np.ndarray):
+        pos = pos//16
+        pos = (pos[0],pos[1])
+        
+        if(not ray.isFast):
+            if self.temp[pos] >= 100:
+                if(random() < 2**-(lenght/3000)):
+                    ray.isFast = False
+                    self.temp[pos] += 10    
+            else:
+                if(random() < 2**-(lenght/60)):
+                    ray.isFast = False
+                    self.temp[pos] += 10
+        return False
     def draw(self, screen):
         for iy, ix in np.ndindex(self.temp.shape):
             pg.draw.rect(screen, (self.temp[iy,ix],50,200), pg.Rect((ix*zone[0]/self.temp.shape[1]+110,iy*zone[1]/self.temp.shape[0]+160),(16,16)))
@@ -76,10 +114,23 @@ class Rod(MatObject):
 class FuelRod(Rod):
     def __post_init__(self):
         self.xenonField = XsenonField(self.n, pos=self.pos)
+        self.space = int((zone[0]-FUEL_ROD_SIZE*self.n)/(self.n+1))
+    
+    
     def draw(self,screen):
         self.xenonField.draw(screen=screen,fuelRod=self)
-    def interact(self, ray:NeutronRay)->tuple:
-        pass
+    def interact(self, ray:NeutronRay, lenght, pos:np.ndarray)->bool:
+        xefield:np.ndarray = self.xenonField.xenonFields[pos[0]//self.space]
+        cord = (int(pos[0]),int(pos[1])) 
+        
+        if(random()<=xefield[cord]/255):
+            return True
+        if(random() < 2**-(lenght/200)):
+            for i in range(-1,2,2):
+                NeutronRay(200,ray.pitc+(math.pi/3)*i,pos,True)
+            xefield[cord] = math.abs(xefield[cord]-5)
+            return True
+        return False 
 
 class XsenonField():
     def __init__(self, n:int, pos:np.ndarray):
@@ -87,7 +138,12 @@ class XsenonField():
         self.n = n
         self.pos = pos
         drawList.append(self)
-        self.randgen()
+    
+    def sum(self):
+        for i in self.xenonFields:
+            i += np.full_like(i,1/CLOCK_TIME)
+            i[np.where(i>255)]=255
+
     def randgen(self):
         for i in self.xenonFields:
             _randgen(i)
@@ -120,21 +176,32 @@ class ControlRod(Rod):
         for i in range(self.n-1):
             xpos = self.pos[0]+(1.5*space + i*(space+FUEL_ROD_SIZE))
             pg.draw.rect(screen,(100,100,100),pg.Rect((xpos,self.pos[1]),(FUEL_ROD_SIZE,int(zone[1]*(1-self.hight)))))
-    def interact(self, ray:NeutronRay)->tuple:
-        pass
+    def interact(self, ray:NeutronRay, lenght, pos:np.ndarray)->tuple:
+        if(random()<0.95):
+            return True
+        return False
 
-class GreateCalculator():
-    def callenght(material, ray:NeutronRay)->bool:
-        pass    
 
+def findObj(color:int)->MatObject:
+    if color == 50:
+        return wf
+    if color == 100:
+        return crod
+    if color == 200:
+        return frod
+    if color == 0:
+        return 
 
 
 origin = (100,150)
 size = (1043,531)
 
 wf = WaterField(32,64)
-frod = FuelRod((origin[0]+10,origin[1]+10),15)
-crod = ControlRod((origin[0]+10,origin[1]+10),15,0.5)
+frod = FuelRod((origin[0]+10,origin[1]+10),ROD_COUNT)
+crod = ControlRod((origin[0]+10,origin[1]+10),ROD_COUNT,1)
+
+for i in range(10):
+    NeutronRay(50,random()*2*math.pi,np.array((600,400)),True)
 
 pg.init()
 screen = pg.display.set_mode((1200, 800))
@@ -142,19 +209,22 @@ pg.display.set_caption('numba',str(os.getpid))
 pg.draw.rect(screen, (0,100,100), pg.Rect(origin, size), 10)
 pg.display.flip()
 
+
 while True:
+    pg.time.Clock().tick(1)
     start_time = time.time()
     #рисуем drawlist
     for i in drawList:
         i.draw(screen)
-    
+    for i in Neutron_list:
+        i.throw(screen)
     #рисуем fps
-    screen.blit(pg.font.SysFont("monospace", 15).render(str(1/(time.time()-start_time)),True,(255,255,255),(0,0,0)),(100,100))
+    screen.blit(pg.font.SysFont("monospace", 15).render(str(1/(time.time()-start_time))+'  '+str(len(Neutron_list)),True,(255,255,255),(0,0,0)),(100,100))
     pg.display.flip()
-    wf.randgen()
-    frod.xenonField.randgen()
-    crod.hight = (math.sin(1/(time.time()-start_time))+1)/2
+    wf.substract()
+    frod.xenonField.sum()
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
+    
