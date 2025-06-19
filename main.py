324bbf82c6ee1d7907394388c621b70084cde5ae
youAt0m <senkaved@gmail.com>
2025-06-19 21:58:05 +0300

    add debug draw mod

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass, field
 import math
-import os
 from random import randint, random
 import time
 from types import NoneType
@@ -15,6 +14,8 @@ Neutron_list=[]
 
 Collision_objects = {}
 
+
+DEBUG=True
 zone=(1024,512)
 FUEL_ROD_SIZE = 10
 XENON_CELL_SIZE = 5
@@ -57,7 +58,8 @@ class NeutronRay():
         for i in sorted(col_objects.keys()):
             n = (self.originVec[0]/i)
             col_vec = self.pos + n*self.originVec
-            
+            if DEBUG:
+                pg.draw.line(screen, (255,0,0),(self.pos[0],self.pos[1]),(col_vec[0],col_vec[1]))
             if 662<col_vec[1]<150:
                 return 
             if self.originObj != col_objects[i]:
@@ -110,6 +112,8 @@ class WaterField():
                     #ray.isFast = False
                     self.temp[pos] += 10
         return False
+    def debug_draw(self,screen:pg.surface.Surface):
+        pass
     def draw(self, screen):
         for iy, ix in np.ndindex(self.temp.shape):
             pg.draw.rect(screen, (self.temp[iy,ix],50,200), pg.Rect((ix*zone[0]/self.temp.shape[1]+110,iy*zone[1]/self.temp.shape[0]+160),(16,16)))
@@ -137,14 +141,17 @@ class FuelRod(Rod):
         self.xenon_field = XsenonField(pos=self.pos)
         Collision_objects[self.pos[0]] = self
         Collision_objects[self.pos[0]+FUEL_ROD_SIZE] = self
-    def draw(self,screen):
-        self.xenon_field.draw(screen=screen)
+    # def draw(self,screen):
+    #     self.xenon_field.draw(screen=screen)
+    def debug_draw(self,screen:pg.surface.Surface):
+        pg.draw.line(screen,(0,255,0),(self.pos[0],0),(self.pos[0],2000))
+        pg.draw.line(screen,(0,255,0),(self.pos[0]+FUEL_ROD_SIZE,0),(self.pos[0]+FUEL_ROD_SIZE,2000))
     def interact(self, ray:NeutronRay, lenght, pos:np.ndarray)->bool:
         pos -= self.pos
         cord = (pos[0]//XENON_CELL_SIZE,pos[1]//XENON_CELL_SIZE)
         if(random()<=self.xenon_field.xenon_field[cord]/255):
             return True
-        if(random() < 2**-(lenght/self.k)):
+        if(random() > 2**-(lenght/self.k)):
             for i in range(-1,2,2):
                 NeutronRay(self,ray.pitc+(math.pi/3)*i,pos,True)
             self.xenon_field.xenon_field[cord] = math.fabs(self.xenon_field.xenon_field[cord]-5)
@@ -164,11 +171,12 @@ class XsenonField():
     
     def randgen(self):
         _randgen(self.xenon_field)
-
+    def debug_draw(self,screen:pg.surface.Surface):
+        pass
     def draw(self, screen:pg.surface.Surface):
         for ix, iy in np.ndindex(self.xenon_field.shape):
             pg.draw.rect(screen, ((100,200,self.xenon_field[ix,iy])),pg.Rect((self.pos[0]+XENON_CELL_SIZE*ix,self.pos[1]+XENON_CELL_SIZE*iy),(XENON_CELL_SIZE,XENON_CELL_SIZE)))    
-            
+    
 
 # @njit(parallel=True)
 def _randgen(array):
@@ -191,13 +199,16 @@ class WaterChannel():
         s.set_alpha(128)
         s.fill((0,0,255))
         screen.blit(s,(self.pos[0]-WATER_CHANNEL_RADIUS,self.pos[1]))
+    def debug_draw(self,screen:pg.surface.Surface):
+        pg.draw.line(screen,(0,0,255),(self.pos[0]-WATER_CHANNEL_RADIUS,0),(self.pos[0]-WATER_CHANNEL_RADIUS,2000))
+        pg.draw.line(screen,(0,0,255),(self.pos[0]+WATER_CHANNEL_RADIUS+FUEL_ROD_SIZE,0),(self.pos[0]+WATER_CHANNEL_RADIUS+FUEL_ROD_SIZE,2000))
 
 class Graphite():
     def __init__(self, k:int):
         self.k = k
         self.lvl = 2
     def interact(self, ray:NeutronRay,lenght, pos:np.ndarray)->tuple:
-        if(random() < 2**-(lenght/self.k)):
+        if(random() > 2**-(lenght/self.k)):
             ray.isFast=False
         return False
 @dataclass
@@ -206,8 +217,13 @@ class ControlRod(Rod):
     def __post_init__(self):
         super().__post_init__()
         self.lvl = 0
+        Collision_objects[self.pos[0]] = self
+        Collision_objects[self.pos[0]+FUEL_ROD_SIZE] = self
     def draw(self,screen):
         pg.draw.rect(screen,(100,100,100),pg.Rect((self.pos[0],self.pos[1]),(FUEL_ROD_SIZE,int(zone[1]*(1-self.hight)))))
+    def debug_draw(self,screen):
+        pg.draw.line(screen,(200,255,200),(self.pos[0],0),(self.pos[0],2000))
+        pg.draw.line(screen,(200,255,200),(self.pos[0]+FUEL_ROD_SIZE,0),(self.pos[0]+FUEL_ROD_SIZE,2000))
     def interact(self, ray:NeutronRay, lenght, pos:np.ndarray)->tuple:
         if(random()<0.95):
             return True
@@ -234,12 +250,11 @@ for i in range(ROD_COUNT-1):
 
 # NeutronRay(g,random()*2*math.pi,np.array((600.0,400.0)),True)
 for i in range(10):
-    NeutronRay(g,random()*2*math.pi,np.array((600.0,400.0)),True)
+    NeutronRay(g,random()*2*math.pi,np.array((597.0,400.0)),True)
 
 
 pg.init()
 screen = pg.display.set_mode((1200, 800))
-pg.display.set_caption('numba',str(os.getpid))
 pg.draw.rect(screen, (0,100,100), pg.Rect(origin, size), 10)
 pg.display.flip()
 
@@ -249,7 +264,10 @@ while True:
     start_time = time.time()
     #рисуем drawlist
     for i in drawList:
-        i.draw(screen)
+        if DEBUG:
+            i.debug_draw(screen)
+        else:
+            i.draw(screen)
     for i in Neutron_list:
         i.throw(screen)
     #рисуем fps
